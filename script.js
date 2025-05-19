29da6d497e58419ec4fb1a064a0590f02ca32534
Miroslav Milanovic <miki.milanovic27@gmail.com>
2025-05-20 01:41:38 +0200

    added getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world!");
+function getComputerChoice(){
+
+    let a = Math.round(Math.random()*1000);
+
+    if(a % 3 === 0){
+        return "rock";
+    }
+    if(a % 3 === 1){
+        return "paper";
+    }
+    if(a % 3 === 2){
+        return "scissors";
+    }
+    return null;
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
