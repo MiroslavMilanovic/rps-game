68f46ea8de971117a461cb197bfde824b622215e
Miroslav Milanovic <miki.milanovic27@gmail.com>
2025-05-22 19:56:21 +0200

    created getHumanChoice function

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
 
     let a = Math.round(Math.random()*1000);
-
     if(a % 3 === 0){
         return "rock";
     }
@@ -14,8 +16,14 @@ function getComputerChoice(){
     return null;
 }
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let humanChoice = prompt("Make a turn!", null);
+    if(humanChoice.toLowerCase() === "rock") return "rock";
+    if(humanChoice.toLowerCase() === "scissors") return "scissors";
+    if(humanChoice.toLowerCase() === "paper") return "paper";
+    return null;
+}
+
+function playRound(humanChoice, computerChoice){
+   
+}
