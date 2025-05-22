8316dc94754f800989386faea34a49b566671f43
Miroslav Milanovic <miki.milanovic27@gmail.com>
2025-05-22 20:23:07 +0200

    created playRound function, modified other functions to return int

--- script.js
+++ script.js
@@ -5,25 +5,40 @@ function getComputerChoice(){
 
     let a = Math.round(Math.random()*1000);
     if(a % 3 === 0){
-        return "rock";
+        console.log("computer choosed rock!");
     }
     if(a % 3 === 1){
-        return "paper";
+       console.log("computer choosed paper!");
     }
     if(a % 3 === 2){
-        return "scissors";
+        console.log("computer choosed scissors!");
     }
-    return null;
+    return a%3;
 }
 
 function getHumanChoice(){
     let humanChoice = prompt("Make a turn!", null);
-    if(humanChoice.toLowerCase() === "rock") return "rock";
-    if(humanChoice.toLowerCase() === "scissors") return "scissors";
-    if(humanChoice.toLowerCase() === "paper") return "paper";
+    if(humanChoice.toLowerCase() === "rock") return 0;
+    if(humanChoice.toLowerCase() === "paper") return 1;
+    if(humanChoice.toLowerCase() === "scissors") return 2;
     return null;
 }
 
 function playRound(humanChoice, computerChoice){
-   
+   if(humanChoice === computerChoice) return;
+   if(humanChoice - computerChoice === -1 || humanChoice - computerChoice === 2) {
+      computerScore++;
+      console.log("You lose this round! Current score " + humanScore + ":" + computerScore);
+      return;
+   }
+   if(humanChoice - computerChoice === 1 || humanChoice - computerChoice === -2) {
+      humanScore++;
+      console.log("You win this round! Current score " + humanScore + ":" + computerScore);
+      return;
+   }
 }
+
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+playRound(humanChoice, computerChoice);
+
